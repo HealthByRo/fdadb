eefad70184e629e1905c8f730b448597129d7864
Marcin Kaszynski <marcin.kaszynski@gmail.com>
2018-11-30 03:58:47 +0100

    Optimize fetch.

--- fdadb/management/commands/fetch_ndc_database.py
+++ fdadb/management/commands/fetch_ndc_database.py
@@ -9,7 +9,9 @@ from django.core.management.base import BaseCommand
 
 from fdadb.models import MedicationName, MedicationNDC, MedicationStrength
 
-FDA_NDC_DATABASE_URL = getattr(settings, "FDA_NDC_DATABASE_URL", "https://www.accessdata.fda.gov/cder/ndctext.zip")
+FDA_NDC_DATABASE_URL = getattr(
+    settings, "FDA_NDC_DATABASE_URL", "https://www.accessdata.fda.gov/cder/ndctext.zip"
+)
 
 
 def strip_list_items(items):
@@ -44,13 +46,21 @@ class Command(BaseCommand):
             return {}
 
         active_substances = strip_list_items(substance_name.split(";"))
-        substances_strength = strip_list_items(product_data["ACTIVE_NUMERATOR_STRENGTH"].split(";"))
-        substances_units = strip_list_items(product_data["ACTIVE_INGRED_UNIT"].split(";"))
-        return OrderedDict({
-            active_substances[i]: {
-                "strength": substances_strength[i], "unit": substances_units[i]
-            } for i in range(len(active_substances))
-        })
+        substances_strength = strip_list_items(
+            product_data["ACTIVE_NUMERATOR_STRENGTH"].split(";")
+        )
+        substances_units = strip_list_items(
+            product_data["ACTIVE_INGRED_UNIT"].split(";")
+        )
+        return OrderedDict(
+            {
+                active_substances[i]: {
+                    "strength": substances_strength[i],
+                    "unit": substances_units[i],
+                }
+                for i in range(len(active_substances))
+            }
+        )
 
     def handle(self, *args, **options):
         # each product has following fields:
@@ -59,21 +69,50 @@ class Command(BaseCommand):
         # * SUBSTANCENAME - names of the substances in the drug, e.g.: CLINDAMYCIN PHOSPHATE; TRETINOIN
         # * ACTIVE_NUMERATOR_STRENGTH - strengths of the substances, e.g.: 12; .25
         # * ACTIVE_INGRED_UNIT - units for the strengths e.g.: mg/g; mg/g
+
+        cache = {}
+
+        def cached_get_or_create(key, model, defaults=None, **kwargs):
+            try:
+                return cache[key], False
+            except KeyError:
+                obj, created = model.objects.get_or_create(defaults=defaults, **kwargs)
+                cache[key] = obj
+                return obj, created
+
         products_data = self.get_products_data()
-        for product in products_data:
-            print("Parsing: ", product["PROPRIETARYNAME"], product["PRODUCTNDC"])
+        skipped = 0
+        for i, product in enumerate(products_data):
+            if (i % 100) == 0:
+                print(f"\rrow {i}", end="", flush=True)
             if product["NDC_EXCLUDE_FLAG"] != "N":
-                print("Skipping excluded drug:", product["PROPRIETARYNAME"], product["PRODUCTNDC"])
+                skipped += 1
                 continue
             product_name = "{} {}".format(
-                product["PROPRIETARYNAME"].strip(), product["PROPRIETARYNAMESUFFIX"].strip()).strip()
+                product["PROPRIETARYNAME"].strip(),
+                product["PROPRIETARYNAMESUFFIX"].strip(),
+            ).strip()
             strength_data = self.get_medication_strength_data(product)
-            medication_name, created = MedicationName.objects.get_or_create(name=product_name, defaults={
-                "active_substances": list(strength_data.keys())
-            })
-            medication_strength, created = MedicationStrength.objects.get_or_create(
-                medication_name=medication_name, strength=strength_data)
-            MedicationNDC.objects.get_or_create(ndc=product["PRODUCTNDC"], defaults={
-                "medication_strength": medication_strength,
-                "manufacturer": product["LABELERNAME"]
-            })
+            medication_name, created = cached_get_or_create(
+                f"name:{product_name}",
+                MedicationName,
+                name=product_name,
+                defaults={"active_substances": list(strength_data.keys())},
+            )
+            medication_strength, created = cached_get_or_create(
+                f"strength:{medication_name.name}-{strength_data}",
+                MedicationStrength,
+                medication_name=medication_name,
+                strength=strength_data,
+            )
+            ndc = product["PRODUCTNDC"]
+            cached_get_or_create(
+                f"ndc:{ndc}",
+                MedicationNDC,
+                ndc=ndc,
+                defaults={
+                    "medication_strength": medication_strength,
+                    "manufacturer": product["LABELERNAME"],
+                },
+            )
+        print(f"\rDone.  {i} rows, {skipped} excluded drugs")
